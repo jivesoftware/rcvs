14fc44e741237cc35f7168c91ed35969a8e8cf4c
Jonathan Colt <jonathan.colt@jivesoftware.com>
2014-10-09 08:06:55 -1000

    added row-column-value-store-marshall-id to detangle dependency on jive-utils id module

---
+++ code/com/jivesoftware/os/rcvs/row-column-value-store-marshall-id/src/main/java/com/jivesoftware/os/rcvs/marshall/id/ImmutableByteArrayMarshaller.java
@@ -0,0 +1,42 @@
+/*
+ * $Revision$
+ * $Date$
+ *
+ * Copyright (C) 1999-$year$ Jive Software. All rights reserved.
+ *
+ * This software is the proprietary information of Jive Software. Use is subject to license terms.
+ */
+package com.jivesoftware.os.rcvs.marshall.id;
+
+import com.jivesoftware.os.jive.utils.id.ImmutableByteArray;
+import com.jivesoftware.os.rcvs.marshall.api.TypeMarshaller;
+
+
+/**
+ *
+ */
+public class ImmutableByteArrayMarshaller implements TypeMarshaller<ImmutableByteArray> {
+
+    public ImmutableByteArrayMarshaller() {
+    }
+
+    @Override
+    public ImmutableByteArray fromBytes(byte[] bytes) throws Exception {
+        return fromLexBytes(bytes);
+    }
+
+    @Override
+    public byte[] toBytes(ImmutableByteArray t) throws Exception {
+        return toLexBytes(t);
+    }
+
+    @Override
+    public ImmutableByteArray fromLexBytes(byte[] lexBytes) throws Exception {
+        return new ImmutableByteArray(lexBytes);
+    }
+
+    @Override
+    public byte[] toLexBytes(ImmutableByteArray t) throws Exception {
+        return t.getImmutableBytes();
+    }
+}
